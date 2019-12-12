fc0c9f7f9debbc7debc84fe9998d647c0ed4e725
Gareth Emslie <gaemslie@microsoft.com>
2019-12-12 17:33:11 +0100

    adding logging and updating async code

--- Tasks/DownloadArtifactsGitHubEnterprise/index.ts
+++ Tasks/DownloadArtifactsGitHubEnterprise/index.ts
@@ -5,24 +5,36 @@ import shell = require("shelljs");
 import fs = require('fs');
 
 async function InitRepo(git : gitP.SimpleGit, gheRepoUrl:string) {
+    tl.debug('Initializing git repository.');
     // Init the git repo folder
     await git.init();
 
+    tl.debug('Disabling git house keeping tasks.');
     // Disable the git housekeeping tasks - https://git-scm.com/docs/git-gc/2.12.0#_options
     await git.addConfig('gc.auto', '0');
-         
+
+    tl.debug('Getting agent proxy configuration.');
+    // Get the proxy configured for the DevOps Agent
     const proxy : tl.ProxyConfiguration | null = tl.getHttpProxyConfiguration();
     // Is a Proxy set?
     if(proxy)
     {
+        tl.debug('Agent proxy is set.');
+
+        // Get THe proxy Url
         var proxyUrl = url.parse(proxy.proxyUrl);
+
         // Is this needed? or is this already included in the url?
         if (proxy.proxyUsername && proxy.proxyPassword) {
             proxyUrl.auth = proxy.proxyUsername + ':' + proxy.proxyPassword;
         }
+        
+        tl.debug('Configuring git proxy.');
+        // Set the proxy for git
         await git.addConfig("http.proxy", url.format(proxyUrl));
     }
 
+    tl.debug('Getting git config for credential-helper.');
     // Make sure we are not using credential helper as the interactive prompt as blocks this task
     const data = await git.raw(
         [
@@ -31,19 +43,20 @@ async function InitRepo(git : gitP.SimpleGit, gheRepoUrl:string) {
             'credential.helper'
         ]);
     
-    tl.debug(`credential.helper = ${ data }`);
-    if(data && data !== "")
+    if(data && data.trim() !== "")
     {
         throw new Error('If credential helper is enabled the interactive prompt can block this task.');
     }
     
+    tl.debug('Setting git config http.sslVerify.');
+    // We should get this from the GHE Service Endpoint configuration!
     await git.addConfig('http.sslVerify', 'false');
 
+    tl.debug(`Adding new remote for origin at '${gheRepoUrl}'.`);
     // Add the git remote repo
     await git.addRemote('origin', gheRepoUrl);
 
-    tl.debug(`Added new remote for origin at '${gheRepoUrl}'.`);
-
+    tl.debug('fetching remote origin.');
     // Fetch git repo from origin
     await git.fetch([
         '--tags', 
@@ -52,9 +65,11 @@ async function InitRepo(git : gitP.SimpleGit, gheRepoUrl:string) {
         '--no-recurse-submodules',
         'origin'
     ]); 
+    tl.debug('Git repository initialization completed succesfully.');
 }
 
 async function run() {
+    console.log(`Downloading artifact.`);
     try {
         const connection : string | undefined = tl.getInput("connection");
         const repository: string | undefined = tl.getInput("definition");
@@ -115,7 +130,7 @@ async function run() {
         tl.debug(`Checking if downloadPath folder '${downloadPath}' exists.`);
         // Create the repo folder if doesnt exist
         if (!fs.existsSync(downloadPath)) {
-            tl.debug(`downloadPath folder '${downloadPath}' does not exist therefore creating folder.`);
+            tl.debug('downloadPath folder does not exist therefore creating folder.');
             shell.mkdir(downloadPath);
         }        
 
@@ -128,39 +143,40 @@ async function run() {
         }
         var gheRepoUrl = url.format(gheRepo)
 
-        tl.debug(`GitHub Enterprise Repo url is '${gheRepoUrl}'`);
+        tl.debug(`GitHub Enterprise Repo url is '${gheRepoUrl}'.`);
 
         const git = gitP(downloadPath)
                         .silent(true)
                         .customBinary(gitPath)
                         .outputHandler((command, stdout, stderr) => {
                             stdout.pipe(process.stdout);
+                            stderr.pipe(process.stderr);
                          });
 
+        tl.debug('Checking git version.');
         // Query Git client version
         await git.raw(
         [
             'version'
         ]);
 
-        console.log(`Downloading artifact.`);
-
         // Init local repo at the download path
         await InitRepo(git, gheRepoUrl);
 
+        tl.debug(`Starting git checkout for desired commit - ${commitId}`);
         // Checkout the specific commit from the repo
-        await git.checkout([
+        const result = await git.checkout([
             '--progress', 
             '--force', 
             commitId
         ]);
 
-        console.log(`Downloading artifact completed.`);
-
+        tl.debug(`Completed git checkout for desired commit - ${commitId}`);
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
+    console.log(`Downloading artifact completed.`);
 }
 
 run();
